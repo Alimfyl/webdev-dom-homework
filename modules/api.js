c5a185a0410e17d610defc67dd365da9dfbe6d92
Almir Ishmukhametov <alimfyl@gmail.com>
2026-03-04 21:36:15 +0500

    1

---
+++ modules/api.js
@@ -0,0 +1,27 @@
+const host = "https://wedev-api.sky.pro/api/v1/almer-ishkmyhametov-new";
+
+export const fetchComments = () => {
+    return fetch(host + "/comments", {
+        method: "GET",
+    }).then((res) => {
+       
+        if (res.status === 500) {
+            throw new Error("Сервер сломался");
+        }
+        return res.json();
+    });
+};
+export const postComment = (text, name) => {
+    return fetch(host + "/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            text: text,
+            name: name,
+        }),
+    }).then((res) => {
+        if (res.status === 400) {
+            throw new Error("Имя и комментарий должны быть не короче 3 символов");
+        }
+        return res.json();
+    });
+};
