d21fe14ab007be1b2191f8e651b7b6a7db18f42b
Almir Ishmukhametov <alimfyl@gmail.com>
2026-03-23 21:06:35 +0500

    1

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,10 @@
 const host = "https://wedev-api.sky.pro/api/v2/almer-ishkmyhametov-new";
+const authHost = "https://wedev-api.sky.pro/api/user"
 
+let token = ""
+export const setToken = (newToken) => {
+    token = newToken
+}
 export const fetchComments = () => {
     return fetch(host + "/comments", {
         method: "GET",
@@ -14,6 +19,9 @@ export const fetchComments = () => {
 export const postComment = (text, name) => {
     return fetch(host + "/comments", {
         method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
         body: JSON.stringify({
             text: text,
             name: name,
@@ -29,3 +37,16 @@ export const postComment = (text, name) => {
         return res.json();
     });
 };
+
+export const login = (login, password) => {
+    return fetch(authHost + '/login'), {
+        method: 'POST',
+        body: JSOW.stringify({ login: login, password: password})
+    }
+}
+export const registration = (name, login, password) => {
+    return fetch(authHost, {
+        method: 'POST',
+        body: JSON.stringify({ name: name,login: login, password: password})
+    })
+}
