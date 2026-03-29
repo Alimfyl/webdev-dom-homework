c14808376ce3a3eae6370b4b8cde74e5c663f4da
Almir Ishmukhametov <alimfyl@gmail.com>
2026-03-29 11:36:51 +0500

    8,1

--- modules/api.js
+++ modules/api.js
@@ -1,9 +1,46 @@
-// ... начало файла без изменений
+const host = "https://wedev-api.sky.pro/api/v2/almer-ishkmyhametov-new";
+const authHost = "https://wedev-api.sky.pro/api/user"
+
+export let userName = null;
+export const setUserName = (newName) => {
+    userName = newName;
+}
+
+export let token = null;
+export const setToken = (newToken) => {
+    token = newToken
+}
+export const fetchComments = () => {
+    return fetch(host + "/comments", {
+        method: "GET",
+    }).then((res) => {
+        if (res.status === 500) throw new Error("Сервер сломался");
+        return res.json();
+    });
+};
+
+export const postComment = (text) => {
+    return fetch(host + "/comments", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`, 
+        },
+        body: JSON.stringify({
+            text: text,
+           
+        }),
+    }).then((res) => {
+        if (res.status === 401) throw new Error("Нет авторизации");
+        if (res.status === 400) throw new Error("Текст должен быть не короче 3 символов");
+        if (res.status === 500) throw new Error("Сервер сломался");
+        return res.json();
+    });
+};
 
 export const loginUser = (login, password) => {
     return fetch(authHost + '/login', {
         method: 'POST',
-        // УДАЛИЛИ headers: { "Content-Type": "application/json" },
+        
         body: JSON.stringify({ login, password })
     }).then((res) => {
         if (res.status === 400) throw new Error("Неверный логин или пароль");
@@ -18,7 +55,7 @@ export const loginUser = (login, password) => {
 export const registration = (name, login, password) => {
     return fetch(authHost, {
         method: 'POST',
-        // УДАЛИЛИ headers: { "Content-Type": "application/json" },
+        
         body: JSON.stringify({
             login: login,
             name: name,      
